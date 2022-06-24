63d38869b62b701e514e7310da39795e1770f2d7
Avery Novick <averynov@yahoo.com>
2022-06-24 16:40:50 -0400

    done

--- starter/level-three.js
+++ starter/level-three.js
@@ -2,3 +2,19 @@
  * DOTS: Level Three
  *
  */
+
+let score = 0
+
+const balls = document.querySelectorAll('.js-ball')
+const scoreDisplay = document.querySelector('.js-score')
+const levelWinner = document.querySelector('.level-winner')
+
+for (let i = 0; i < balls.length; i++) {
+  balls[i].addEventListener('click', () => {
+    score = score + parseInt(balls[i].getAttribute('data-increment'))
+    scoreDisplay.innerText = score
+    if (score >= 100) {
+      levelWinner.style.opacity = '1'
+    }
+  })
+}
--- starter/level-two.js
+++ starter/level-two.js
@@ -2,3 +2,18 @@
  * DOTS: Level Two
  *
  */
+let score = 0
+
+const balls = document.querySelectorAll('.js-ball')
+const scoreDisplay = document.querySelector('.js-score')
+const levelWinner = document.querySelector('.level-winner')
+
+for (let i = 0; i < balls.length; i++) {
+  balls[i].addEventListener('click', () => {
+    score = score + 10
+    scoreDisplay.innerText = score
+    if (score >= 100) {
+      levelWinner.style.opacity = '1'
+    }
+  })
+}
